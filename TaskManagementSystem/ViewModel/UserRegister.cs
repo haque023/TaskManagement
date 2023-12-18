56fa5e5db2b3ec6de2dbc537a34b6ea5c2198835
Emdadul Haque <Emdad-Pc@DESKTOP-0SM6RSD>
2023-12-18 17:44:13 +0600

    first commit

---
+++ TaskManagementSystem/ViewModel/UserRegister.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementSystem.ViewModel
+{
+    public class UserRegister
+    {
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 4)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 4)]
+        public string ConfirmPassword { get; set; }
+    }
+}
